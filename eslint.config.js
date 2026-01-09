408209edbd4e951be7a8044ce01c86def3034ea8
gdgeek <dirui1@yeah.net>
2026-01-09 23:08:48 +0800

    Fix CI warnings: upgrade ESLint and TypeScript compatibility, suppress punycode deprecation warnings
    - Upgrade @typescript-eslint/eslint-plugin from 6.13.1 to 8.52.0
    - Upgrade @typescript-eslint/parser from 6.13.1 to 8.52.0
    - Upgrade eslint from 8.54.0 to 9.39.2
    - Migrate from .eslintrc.js to eslint.config.js for ESLint v9 compatibility
    - Add NODE_OPTIONS='--no-deprecation' to test scripts to suppress punycode warnings
    - Update cos-nodejs-sdk-v5 to latest version 2.15.4
    - Fix validation.ts formatting issue
    
    All linting errors resolved, TypeScript version compatibility restored, and deprecation warnings eliminated.

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+const js = require('@eslint/js');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+const prettier = require('eslint-plugin-prettier');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+        global: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-console': 'warn',
+      'no-undef': 'off',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', '*.js', '!eslint.config.js'],
+  },
+];
